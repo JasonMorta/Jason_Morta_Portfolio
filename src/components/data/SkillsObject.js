d9806e7ecbf0421f4e45ee1916216236ed3c91c5
JasonMorta <jason.morta23@gmail.com>
2022-08-16 17:17:06 +0200

    scroll update

--- src/components/data/SkillsObject.js
+++ src/components/data/SkillsObject.js
@@ -3,6 +3,13 @@ import html5 from '../../img/html5.svg'
 import css from '../../img/CSS.svg'
 import jQ from '../../img/jquery.png'
 import boot from '../../img/boots.svg'
+import mongo from '../../img/mongodb.svg'
+import Git from '../../img/gitHub.svg'
+import express from '../../img/express.png'
+import next from '../../img/next.png'
+import mui from '../../img/MUi.png'
+import node from '../../img/node.png'
+import react from '../../img/react.png'
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
    {
@@ -13,7 +20,7 @@ export default [
          Writing much cleaner and reusable code.</p>]
    },
    {
-      id: 'css',
+      id: 'CSS3',
       title: 'CSS3',
       skill_img: css,
       description: [<p><b>CSS3: </b> 
@@ -21,22 +28,65 @@ export default [
       I'm constantly improving my style and designs to create attractive designs.</p>]
    },
    {
-      id: 'js',
+      id: 'JavaScript',
       title: 'JavaScript',
       skill_img: js,
       description: [<p><b>Javascript:</b> I regularly use JS in my projects, using some of
       the latest ES-version syntax like for of, forEach, arrow functions, ternary operator, template literals, and more.</p>]
    },
    {
-      id: 'jq',
+      id: 'jQuery',
       title: 'jQuery',
       skill_img: jQ,
-      description: `JQUERY: This library makes adding animation with JS much easier and faster. Because of its support on all modern browsers I tend to use jQuery more often.`
+      description: `jQuery: Adding animations with JS made easy and fast. Because of its support on all modern browsers it is a good tool for any html,css and JS project.`
    },
    {
-      id: 'bs',
+      id: 'Bootstrap',
       title: 'Bootstrap',
       skill_img: boot,
-      description: `BOOTSTRAP: To quickly create responsive tables, images, or forms, I turn to bootstrap but add my own styling to give my code a personal touch.`
+      description: `Bootstrap: To quickly create responsive tables, images, or forms, I turn to bootstrap but add my own styling to give my code a personal touch.`
    },
+   {
+      id: 'GitHub',
+      title: 'GitHub',
+      skill_img: Git,
+      description: `GitHub: I'm constantly making use of gitHub, uploading all my projects with the terminal or desktop application.`
+   },
+   {
+      id: 'Material',
+      title: 'Material UI',
+      skill_img: mui,
+      description: `Material UI: Similar to bootstrap, it has a long list of free react components and has much more customizable and styling features.`
+   },
+   {
+      id: 'Node',
+      title: 'Node.js',
+      skill_img: node,
+      description: `Node.js: I use node to create my back-end server for my applications. I'm always trying out different node modules to improve my server.`
+   },
+   {
+      id: 'MongoDB',
+      title: 'MongoDB',
+      skill_img: mongo,
+      description: `MongoDB: I use mongodb with most of my server side projects to store any user data and input. And apply some security modules like Helmet.`
+   },
+   {
+      id: 'Express',
+      title: 'Express',
+      skill_img: express,
+      description: `Express framework: With express i'm able to make use of many useful middleware for security and to make communication between my client and server for flexible`
+   },
+   {
+      id: 'next',
+      title: 'next.js',
+      skill_img: next,
+      description: `Next.js: Is great for static and server‑rendered applications. So far I've use use to create three applications`
+   },
+   {
+      id: 'React',
+      title: 'React',
+      skill_img: react,
+      description: `React: I'm very comfortable with react, making use of several hooks and also very well familiar with react class components. `
+   },
+   
 ]
--- src/components/sections/education/Education.jsx
+++ src/components/sections/education/Education.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import skills from '../../data/SkillsObject'
+import './style.css'
+import scrollRight from '../../../img/right.png'
+import scrollLeft from '../../../img/left.png'
+
+
+
+
 
 export default function Education() {
 
 
 
    const mySkills = skills.map((skill, index) => {
-      return   <div className="skill-card"  key={index}>
+      return   <div className="skill-card" id={skill.id}  key={index}>
                   <div className='image-container'>
-                     <img src={skill.skill_img} alt="skill pic"/>
+                     <img width='40' src={skill.skill_img} alt="skill pic"/>
                   </div>
                   <p>{skill.description}
                   </p>
@@ -23,15 +30,15 @@ export default function Education() {
          <em style={{color: 'gray'}}>Education</em>
          <p>In 2009 I finished high school and started working as a machine operator at QTec Moulding. I took a few online classes when I started(2019) my web dev career, but didn't get as much out of it as I have hoped for. I'm now(April 2022) enrolled at <a href="https://www.hyperiondev.com/">Hyperion Dev's</a> on-site boot camp.
          </p>
+         <a className='resume-link' href="https://drive.google.com/file/d/1Ht3eDu1piX8iMPWNUk8xUUHvC1f-pNn7/view?usp=sharing" target="_blank" rel="noreferrer">🔗PDF Resume</a>
       </header>
 
       <h3>Skill Stack</h3>
 
       <div className="contentContainer" id="stacks">
-         
+         <img className='left-arrow' src={scrollLeft} alt='scroll button'/>
          {mySkills}
-        
-         
+         <img className='right-arrow' src={scrollRight} alt='scroll button'/>
       </div>
    </div>
 </section>
