d153bbdceacf6935e8ef70bd6c68201206bbd0dc
JasonMorta <jason.morta23@gmail.com>
2022-08-17 11:39:17 +0200

    analytics actions updated

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import { useNavigate } from "react-router-dom";
 import ReactGA from 'react-ga'
@@ -5,7 +6,6 @@ import ReactGA from 'react-ga'
 export default function Navigation() {
 
    let navigate = useNavigate();
-   
    //Tell google analytics what button was clicked
    function events(cat,act){
          ReactGA.event({
@@ -14,28 +14,40 @@ export default function Navigation() {
          })
    }
 
-  
 
   return (
    <nav className="nav" id="top-nav">
       <div className="nav-container">
          <a className="active" href="#main" onClick={()=>{
-            navigate("./")
-            events("nav button", "clicked on main link")
+            navigate("./");
+            events("Button", "main");
             } }>
             <h4>MAIN</h4>
          </a>
-         <a className="active" href="#work" onClick={() => navigate("./")}>
-            <h4  >MY WORK</h4>
+         <a className="active" href="#work" onClick={() => {
+            navigate("./");
+            events("Button", "My Work");
+            }}>
+            <h4 >MY WORK</h4>
          </a>
-         <a href="#about" onClick={() => navigate("./")}>
+         <a href="#about" onClick={() => {
+            navigate("./");
+            events("Button", "About");
+            }}>
             <h4>ABOUT ME</h4>
          </a>
-         <a href="#contact" onClick={() => navigate("./")}>
+         <a href="#contact" onClick={() => {
+            navigate("./");
+            events("Button", "Contact");
+            }}>
             <h4>CONTACT</h4>
          </a>
          
-         <a onClick={() => navigate("/blogposts")} >
+         <a onClick={() => {
+            navigate("/blogposts");
+            events("Button", "Blogposts");
+            } } >
+
             <h4 >BLOGS</h4>
          </a>
          
