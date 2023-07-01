34bd97c9ceb64e3f01398c71e2367906784f1854
Jason M <46291780+JasonMorta@users.noreply.github.com>
2023-07-01 13:38:21 +0200

    updates

--- src/components/data/SkillsObject.js
+++ src/components/data/SkillsObject.js
@@ -10,56 +10,133 @@ import next from '../../img/next.png'
 import mui from '../../img/MUi.png'
 import node from '../../img/node.png'
 import react from '../../img/react.png'
+import firebase from '../../img/firebase.png'
+import sass from '../../img/sass.png'
+import dropbox from '../../img/dropbox.png'
+import figma from '../../img/figma.png'
+import chrome from '../../img/chrome.png'
+import py from '../../img/python.png'
+import MySQL from '../../img/MySQL.png'
+import php from '../../img/php.png'
+import ts from '../../img/typescript.png'
+import vue from '../../img/vue.png'
+
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
    {
       id: 'html5',
       title: 'HTML5',
-      skill_img: html5   },
+      skill_img: html5
+   },
    {
       id: 'CSS3',
       title: 'CSS3',
-      skill_img: css   },
+      skill_img: css
+   },
    {
       id: 'JavaScript',
       title: 'JavaScript',
-      skill_img: js   },
+      skill_img: js
+   },
    {
       id: 'jQuery',
       title: 'jQuery',
-      skill_img: jQ   },
+      skill_img: jQ
+   },
    {
       id: 'Bootstrap',
       title: 'Bootstrap',
-      skill_img: boot   },
+      skill_img: boot
+   },
    {
       id: 'GitHub',
       title: 'GitHub',
-      skill_img: Git   },
+      skill_img: Git
+   },
    {
       id: 'Material',
       title: 'Material UI',
-      skill_img: mui   },
+      skill_img: mui
+   },
    {
       id: 'Node',
       title: 'Node.js',
-      skill_img: node   },
+      skill_img: node
+   },
    {
       id: 'MongoDB',
       title: 'MongoDB',
-      skill_img: mongo   },
+      skill_img: mongo
+   },
    {
       id: 'Express',
       title: 'Express',
-      skill_img: express   },
+      skill_img: express
+   },
    {
       id: 'next',
       title: 'next.js',
-      skill_img: next   },
+      skill_img: next
+   },
    {
       id: 'React',
       title: 'React',
-      skill_img: react   },
-
+      skill_img: react
+   },
+   {
+      id: 'firebase',
+      title: 'firebase',
+      skill_img: firebase
+   },
+   {
+      id: 'sass',
+      title: 'sass',
+      skill_img: sass
+   },
+   {
+      id: 'dropbox',
+      title: 'dropbox',
+      skill_img: dropbox
+   },
+   {
+      id: 'figma',
+      title: 'figma',
+      skill_img: figma
+   },
+   {
+      id: 'chrome',
+      title: 'chrome',
+      skill_img: chrome
+   },
+   {
+      id: 'python',
+      title: 'python',
+      skill_img: py
+   },
+   {
+      id: 'MySQL',
+      title: 'MySQL',
+      skill_img: MySQL
+   },
+   {
+      id: 'php',
+      title: 'php',
+      skill_img: php
+   },
+   {
+      id: 'typescript',
+      title: 'typescript',
+      skill_img: ts
+   },
+   {
+      id: 'vue',
+      title: 'vue',
+      skill_img: vue
+   },
    
+
+
+
+
 ]
